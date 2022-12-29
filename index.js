8a7eeecdc469c9ad0b136161550fc46cb07c0b7c
manosakpujiha <manos.akpujiha@gmail.com>
2022-12-29 08:24:55 -0500

    fix

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const file = document.querySelector('#file');
 const book = document.querySelector('#book');
 const check = document.querySelector('#check');
 
+const formData = {};
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     console.log('creditCard:', creditCard.value);
@@ -13,23 +15,33 @@ form.addEventListener('submit', function(e) {
     console.log(formData);
 })
 
-const formData = {};
-check.addEventListener('input', function(e) {
-   formData['check'] = e.target.checked;
-   console.log(formData);
-});
+// check.addEventListener('input', function(e) {
+//    formData['check'] = e.target.checked;
+//    console.log(formData);
+// });
 
-creditCard.addEventListener('input', function(e) {
-    formData['credit'] = e.target.value;
-    console.log(formData);
- })
+// creditCard.addEventListener('input', function(e) {
+//     formData['credit'] = e.target.value;
+//     console.log(formData);
+//  });
 
- book.addEventListener('input', function(e) {
-    formData['book'] = e.target.value;
-    console.log(formData);
- });
+//  book.addEventListener('input', function(e) {
+//     formData['book'] = e.target.value;
+//     console.log(formData);
+//  });
 
- file.addEventListener('input', function(e) {
-    formData['file'] = e.target.value;
-    console.log(formData);
- })
+//  file.addEventListener('input', function(e) {
+//     formData['file'] = e.target.value;
+//     console.log(formData);
+//  });
+
+ for (let input of [creditCard, file, book, check]) {
+    input.addEventListener('change', ({target}) => {
+        if (target.type === 'checkbox') {
+            formData[target.name] = target.checked ;
+        } else {
+            formData[target.name] = target.value
+        }
+        console.log(formData);
+    });
+ }
